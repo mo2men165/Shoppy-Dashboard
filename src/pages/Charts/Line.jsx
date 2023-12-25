3e6a1efc5b8d51c4ab78a0b49373c2da02e8eb4e
mo2men165 <moamenabdeltawab@gmail.com>
2023-12-25 08:14:15 +0200

    Initial

---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Header, LineChart } from '../../components'
+
+
+const Line = () => {
+  return (
+    <div className='m-2 md:m-10 mt-24 sm:m-20 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg'>
+      <Header category={'Chart'} title={"Inflation rate"} />
+      <div className='w-full'>
+      <LineChart />
+      </div>
+    </div>
+  )
+}
+
+export default Line
